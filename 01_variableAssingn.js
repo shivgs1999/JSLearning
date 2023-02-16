273c4cf8084851ec357473354aa76f9acbe3597f
shivgs1999 <shivgs1999@gmail.com>
2023-02-16 12:22:33 +0530

    01_Assingn Is completed

---
+++ 01_variableAssingn.js
@@ -0,0 +1,42 @@
+console.log("********************** Step 01 **********************");
+
+var myFullName; //Veriable Declaration 
+console.log(myFullName); //value loged before Inilization
+
+myFullName = "Shivam Suradkar";
+console.log("My Full Name is: ", myFullName);
+
+myFullName = "Shivam Gajanan Suradkar";
+console.log("My Full Name with Middle Name is: ", myFullName);
+
+console.log("********************** Step 02 **********************");
+
+var myLove = "I Love only JS";
+console.log(myLove);
+
+console.log("********************** Step 03 **********************");
+
+var firstName = "Shivam";
+console.log("my First Name:",firstName);
+
+var lastName = "Suradkar";
+console.log("my LAst Name:",lastName);
+
+var age = "23";
+console.log("my Age is:",age);
+
+var address = "At Post Shirpur,TQ & Dist Buldana";
+console.log("my Address is:",address);
+
+var pinCode = "443001";
+console.log("my Pin Code is:",pinCode);
+
+var gender = "Male";
+console.log("my Gender is:",gender);
+
+var isMarried = "No";
+console.log("Marital Status:",isMarried);
+
+console.log("*********I can Write Output in one line also**********");
+
+console.log("My Full Name is: ",myFullName,"My Full Name with Middle Name is: ", myFullName,"my First Name:", firstName, "my LAst Name:",lastName, "my Age is:",age, "my Address is:",address, "my Pin Code is:",pinCode, "my Gender is:",gender, "Marital Status:",isMarried);
--- helloWorld.js
+++ helloWorld.js
@@ -1,1 +1,24 @@
 console.log(" Shivam Suradkar ");
+console.log(23);
+console.log("Buldana");
+
+var fullName = "Shivam Suradkar";
+console.log(fullName);
+
+var city; //Veriable Declaration 
+console.log(city); 
+
+city = "Pune" // veriable inilization
+console.log(city);
+
+city = "surat"  //veriable Update
+console.log(city);
+
+var age = "23"; //veriable Declearization and initialization in same line.
+age = 24;
+age = 25;
+console.log(age);
+
+var university; // Variable Declearization
+university = "SPPU" //Variable inilization
+console.log("my University name is:",university);
